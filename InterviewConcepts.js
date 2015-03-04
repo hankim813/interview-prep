fdad9f8da3a81196b1171399f3e2c0591d228c51
Han Kim <hankim813@gmail.com>
2015-03-03 21:52:40 -0800

    add more scalability concepts

--- InterviewConcepts.js
+++ InterviewConcepts.js
@@ -112,3 +112,123 @@
 // ==========================================================
 // HTTP request caching(proxy servers, etc.), enhanced server caching (memcached, etc.), progressive CSS/JS/image loading. DB Shards
 
+// ==========================================================
+// HashTable
+// ==========================================================
+
+// Hashtable is basically a datastructure to retain values of key-value pair.
+
+// It doesn't allow null for both key and value. You will get NullPointerException if you add null value.
+// It is synchronized. So it comes with its cost. Only one thread can access in one time.
+// Hashtable<Integer,String>; cityTable = new Hashtable<Integer,String>();
+// cityTable.put(1, "Lahore");
+// cityTable.put(2, "Karachi");
+// cityTable.put(3, null); // NullPointerEcxeption at runtime
+ 
+// System.out.println(cityTable.get(1));
+// System.out.println(cityTable.get(2));
+// System.out.println(cityTable.get(3));
+
+// ==========================================================
+// HashMap
+// ==========================================================
+
+// Like Hashtable it also accepts key value pair.
+
+// It allows null for both key and value (only one for key, and multiple for values).
+// It is unsynchronized. So comes up with better performance. Not thread safe.
+
+// HashMap<Integer,String> productMap = new HashMap<Integer,String>();
+// productMap.put(1, "Keys");
+// productMap.put(2, null);
+
+// ==========================================================
+// HashSet
+// ==========================================================
+
+// HashSet does not allow duplicate values. It provides add method rather put method. You also use its contain method to check whether the object is already available in HashSet. HashSet can be used where you want to maintain a unique list.
+
+// HashSet<String> stateSet = new HashSet<String>();
+// stateSet.add ("CA");
+// stateSet.add ("WI");
+// stateSet.add ("NY");
+ 
+// if (stateSet.contains("PB"))  //if CA, it will not add but shows following message
+//      System.out.println("Already found");
+// else
+//     stateSet.add("PB");
+
+// ==========================================================
+// Vertical Scaling
+// ==========================================================
+
+// Vertical scaling, or improving the capabilities of a node/server, gives greater capacity to the node but does not decrease the overall load on existing members of the cluster. That is, the ability for the improved node to handle existing load is increased, but the load itself is unchanged. Reasons to scale vertically include increasing IOPS, increasing CPU/RAM capacity, and increasing disk capacity.
+
+// ==========================================================
+// Horizontal Scaling
+// ==========================================================
+
+// Horizontal scaling, or increasing the number of nodes in the cluster, reduces the responsibilities of each member node by spreading the keyspace wider and providing additional endpoints for client connections. That is, the capacity of each individual node does not change, but its load is decreased. Reasons to scale horizontally include increasing I/O concurrency, reducing the load on existing nodes, and increasing disk capacity.
+
+// ==========================================================
+// Profiling Code and identifying performance bottlenecks and access patterns
+// ==========================================================
+
+// Check response times of api requests
+// Instead of making multiple requests for javascript files or images, you can minify, compress
+// Redundant requests
+// Refactor
+
+// ==========================================================
+// Batch Requests
+// ==========================================================
+
+// Minimize messages between client and server to reduces network traffic and provides a smoother, less chatty user interface. Batch requests allow grouping multiple operations, as described in [OData:Core], into a single HTTP request payload.
+
+// ==========================================================
+// Load Balancing
+// ==========================================================
+
+// In computing, load balancing distributes workloads across multiple computing resources, such as computers, a computer cluster, network links, central processing units or disk drives. Load balancing aims to optimize resource use, maximize throughput, minimize response time, and avoid overload of any single resource. Using multiple components with load balancing instead of a single component may increase reliability through redundancy. Load balancing usually involves dedicated software or hardware, such as a multilayer switch or a Domain Name System server process.
+
+// Load balancing differs from channel bonding in that load balancing divides traffic between network interfaces on a network socket (OSI model layer 4) basis, while channel bonding implies a division of traffic between physical interfaces at a lower level, either per packet (OSI model Layer 3) or on a data link (OSI model Layer 2) basis.
+
+// ==========================================================
+// Server Affinity
+// ==========================================================
+
+// Server affinity refers to the ability of a load balancer or router to send a user's request to the same server where their session was initiated. If session-failover is supported by the application or networking layout, server affinity is not required, as it doesn't matter which server serves the request.
+
+// ==========================================================
+// Connection Pooling
+// ==========================================================
+
+// Opening/Closing database connections is an expensive process and hence connection pools improve the performance of execution of commands on a database for which we maintain connection objects in the pool. It facilitates reuse of the same connection object to serve a number of client requests. Every time a client request is received, the pool is searched for an available connection object and it's highly likely that it gets a free connection object. Otherwise, either the incoming requests are queued or a new connection object is created and added to the pool (depending upon how many connections are already there in the pool and how many the particular implementation and configuration can support). As soon as a request finishes using a connection object, the object is given back to the pool from where it's assigned to one of the queued requests (based on what scheduling algorithm the particular connection pool implementation follows for serving queued requests). Since most of the requests are served using existing connection objects only so the connection pooling approach brings down the average time required for the users to wait for establishing the connection to the database.
+
+// ==========================================================
+// Data Sharding
+// ==========================================================
+
+// When you have servers responsible for holding a subset of the database.
+
+
+// ==========================================================
+// SQL vs NoSQL
+// ==========================================================
+
+// Many NoSQL solutions sacrifice ACID(Atomicity, Consistency, Isolation, Durability) compliancy for performance and scalability
+
+// SQL Scaling: Scaling is vertical. In essence, more data means a bigger server, which can get very expensive. It is possible to scale an RDBMS across multiple servers, but this is a difficult and time-consuming process.
+// NoSQL Scaling: Scaling is horizontal, meaning across servers. These multiple servers can be cheap commodity hardware or cloud instances, making it alot more cost-effective than vertical scaling. Many NoSQL technologies also distribute data across servers automatically.
+
+// Schemas and Flexibility: 
+// SQL: Each record conforms to fixed schema, meaning the columns must be decided and locked before data entry and each row must contain data for each column. This can be amended, but it involves altering the whole database and going offline.
+// NoSQL: Schemas are dynamic. Information can be added on the fly, and each ‘row’ (or equivalent) doesn’t have to contain data for each ‘column’.
+
+// The idea that SQL and NoSQL are in direct opposition and competition with each other is flawed one, not in the least because many companies opt to use them concurrently. As with all of the technologies I’ve previously discussed, there really isn’t a ‘one-system-fits-all’ approach; choosing the right technology hinges on the use case. If your data needs are changing rapidly, you need high throughput to handle viral growth, or your data is growing fast and you need to be able to scale out quickly and efficiently, maybe NoSQL is for you. But if the data you have isn’t changing in structure and you’re experiencing moderate, manageable growth, your needs may be best met by SQL technologies. Certainly, SQL is not dead yet.
+
+// ==========================================================
+// Data Indexing
+// ==========================================================
+
+// A database index is a data structure that improves the speed of data retrieval operations on a database table at the cost of additional writes and storage space to maintain the index data structure.
--- algorithms.js
+++ algorithms.js
@@ -957,3 +957,183 @@ function permute(input) {
   return permutatedArray
 }
 
+// any time if you reference an object, it is just a reference. so if that value changes later in the future so will 
+// the value you set it to.
+
+// ====================================================================
+// Hash Tables
+// ====================================================================
+// var table = makeHashTable();
+// table.insert();
+
+var makeHashTable = function(){
+  var result = {};
+  var storage = [];
+  var storageLimit = 1000;
+  result.insert = function(){
+
+    var index = getIndexBelowMaxForKey(key, storageLimit);
+    storage[index] = storage[index] || [];
+    var pairs = storage[index];
+    var pair;
+    var replaced = false;
+    for (var i = 0; i < pairs.length; i++) {
+      pair = pairs[i];
+      if (pair[0] === key) {
+        pair[1] = value;
+        replaced = true;
+      }
+    }
+
+    if (!replaced) {
+      pairs.push([key, value]);
+    }
+  };
+
+  result.retrieve = function(){
+    var index = getIndexBelowMaxForKey(key, storageLimit);
+    var pairs = storage[index];
+    if (!pairs) { return; }
+    var pair;
+    for (var i = 0; i < pairs.length; i++) {
+      pair = pairs[i];
+      if (pair && pair[0] === key) {
+        return pair[1];
+      }
+    }
+  };
+
+  result.remove = function(){
+    var index = getIndexBelowMaxForKey(key, storageLimit);
+    var pairs = storage[index];
+    var pair;
+    for (var i = 0; i < pairs.length; i++) {
+      pair = pairs[i];
+      if (pair[0] === key) {
+        var value = pair[1];
+        delete pairs[i];
+        return value;
+      }
+    }
+  };
+
+  return result;
+};
+
+// This is a "hashing function". You don't need to worry about it, just use it
+// to turn any string into an integer that is well-distributed between
+// 0 and max - 1
+var getIndexBelowMaxForKey = function(str, max){
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
+
+// ====================================================================
+// Hash Tables with Resizing Index
+// ====================================================================
+
+var makeHashTable = function(){
+  var result = {};
+  var storage = [];
+  var storageLimit = 4;
+  var size = 0;
+  result.insert = function(){
+    var index = getIndexBelowMaxForKey(key, storageLimit);
+    storage[index] = storage[index] || [];
+    var pairs = storage[index];
+    var pair;
+    var replaced = false;
+    for (var i = 0; i < pairs.length; i++) {
+      pair = pairs[i];
+      if (pair[0] === key) {
+        pair[1] = value;
+        replaced = true;
+      }
+    }
+
+    if (!replaced) {
+      pairs.push([key, value]);
+      size++;
+    }
+    if(size >= storageLimit * 0.75){
+      // increase the size of the hash table
+      resize(storageLimit * 2);
+    }
+  };
+
+  result.retrieve = function(){
+    var index = getIndexBelowMaxForKey(key, storageLimit);
+    var pairs = storage[index];
+    if (!pairs) { return; }
+    var pair;
+
+    for (var i = 0; i < pairs.length; i++) {
+      pair = pairs[i];
+      if (pair && pair[0] === key) {
+        return pair[1];
+      }
+    }
+  };
+
+  result.remove = function(){
+    var index = getIndexBelowMaxForKey(key, storageLimit);
+    var pairs = storage[index];
+    var pair;
+
+    for (var i = 0; i < pairs.length; i++) {
+      pair = pairs[i];
+      if (pair[0] === key) {
+        var value = pair[1];
+        delete pairs[i];
+        size--;
+        if(size <= storageLimit * 0.25){
+          // decrease the size of the hash table
+          resize(storageLimit / 2);
+        }
+        return value;
+      }
+    }
+  };
+
+  var resizing = false;
+  function resize(newSize){
+    if(!resizing){
+      resizing = true;
+      var pairs = [];
+      for(var i = 0; i < storage.length; i++){
+        if(!storage[i]) continue;
+        for(var j = 0; j < storage[i].length; j++){
+          if(!storage[i][j]) continue;
+          pairs.push(storage[i][j]);
+        }
+      }
+      storageLimit = newSize;
+      storage = [];
+      size = 0;
+      for(var i = 0; i < pairs.length; i++){
+        result.insert(pairs[i][0], pairs[i][1]);
+      }
+      resizing = false;
+    }
+  }
+
+  return result;
+};
+
+// This is a "hashing function". You don't need to worry about it, just use it
+// to turn any string into an integer that is well-distributed between
+// 0 and max - 1
+var getIndexBelowMaxForKey = function(str, max){
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash<<5) + hash + str.charCodeAt(i);
+    hash = hash & hash; // Convert to 32bit integer
+    hash = Math.abs(hash);
+  }
+  return hash % max;
+};
